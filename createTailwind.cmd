028ca1034b1f71312ba65e1f7995ea36ef943506
Christian Heinisch <chris@christian-heinisch.de>
2025-08-28 22:15:06 +0200

    Update Tailwind CSS, footer, and theme config; add build script
    Regenerated Tailwind CSS files with updated utility classes and properties. Modified footer.php and theme.json for improved layout or configuration. Added createTailwind.cmd script to automate Tailwind CSS build process. Updated html/index.html for consistency with new styles.

---
+++ createTailwind.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+echo Create HTML CSS
+C:\users\chris\Documents\Tailwind\tailwind-projekt\tailwindcss-windows-x64.exe -i html/css/config.css -o html\css\tailwind.css --minify
+
+
+echo Create Automad CSS
+ C:\users\chris\Documents\Tailwind\tailwind-projekt\tailwindcss-windows-x64.exe -i html/css/config.css -o automad\heimfisch\keepreading_ii\css\tailwind.css --minify
